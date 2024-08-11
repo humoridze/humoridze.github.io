9da121cb6c02e283482cb6389cb51ffc5ae2dc2c
humoridze <90176958+humoridze@users.noreply.github.com>
2024-08-11 04:13:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const codeLines = [
     "    <div id=\"content\" class=\"hidden\">",
     "        <div class=\"content-inner\">",
     "            <div class=\"logo\"></div>",
-    "            <p class=\"description\">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>",
+    "            <p class=\"description\">Sed ut perspiciatis unde omnis iste natus error</p>",
     "            <div class=\"buttons\">",
     "                <a href=\"https://t.me/legendagachimuchi\" class=\"btn\">Telegram</a>",
     "                <a href=\"https://github.com/humoridze\" class=\"btn\">GitHub</a>",
